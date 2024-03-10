ef47ef0908dda4c7e3704afd04f6cc74f7a25660
Otávio <o.augusto.brito@gmail.com>
2024-03-10 09:33:36 -0300

    commit

--- EXERCICIOS PARA RELEMBRAR/3.py
+++
@@ -1,6 +0,0 @@
-# Converter temperatura de Celsius para Fahrenheit: 
-# Escreva um programa que converta uma temperatura em graus Celsius para Fahrenheit.
-
-celsius=float(input('Enter a temperature in celsius:'))
-fahrenheit=celsius*1.8+32
-print(f'{fahrenheit:.2f}')
